0927f2013a4c7c3665aae0152dc86dbb38238711
JAME <jame@ssmaule.cl>
2022-12-13 11:36:48 -0300

    Laboratorio Nro.3

--- src/index.js
+++ src/index.js
@@ -26,11 +26,13 @@ const router = createBrowserRouter([
   },  
 ]);
 
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
